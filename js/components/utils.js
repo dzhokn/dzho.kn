853be69c7ddcc80c1f14d46d25b62da92aa241a5
Miroslav Dzhokanov <mdzhokanov@gmail.com>
2025-02-21 15:14:27 +0200

    Fix a layout issue with the header logo (due to delay in loading Dancing-Script font). And extract the initFontLoading() JS logic into utils.js file

--- js/components/utils.js
+++ js/components/utils.js
@@ -13,4 +13,18 @@ export function initGoTop() {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
+}
+
+export function initFontLoading() {
+  const fontPromise = document.fonts.load('1em "Dancing Script"');
+  
+  fontPromise.then(() => {
+    requestAnimationFrame(() => {
+      document.body.classList.add('loaded');
+    });
+  }).catch(() => {
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+    }, 1000);
+  });
 } 
